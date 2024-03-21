719cb77f966c9e6e7d2348565b14d90802b05760
Liss <lissetchopper@gmail.com>
2024-03-20 18:55:36 -0600

    Feat:: crear proyeto VUE y analisis de estructura

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,32 @@
+ <!--  <template>
+aqui va codigo html , etiquetas,div,input,h1  
+</template> --> 
+
+<!-- <script lang="ts" lang ts indica que se usa type script> 
+Logica, datos, funcionamiento
+</script>-->
+
+<!-- <style>
+estilos
+</style>-->
+
+<template>
+
+  <h1>Hola mundo!</h1>
+
+</template>
+
+<script lang="ts">
+</script>
+
+<style>
+h1 {
+  color:purple;
+}
+</style>
+
+
+<!--
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
@@ -25,3 +54,4 @@ export default defineComponent({
   margin-top: 60px;
 }
 </style>
+-->
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
-})
+}) 
+//Aqui se agregan plugins o configuracion de comportamiento de vue
+// tambien se configura typecript servidores y seguridad 
